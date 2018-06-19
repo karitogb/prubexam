2d8a886192da865f657abaa6f432d854597f6a31
karitogb <40370649+karitogb@users.noreply.github.com>
2018-06-18 21:02:36 -0500

    Create ejer1.py

---
+++ ejer1.py
@@ -0,0 +1,6 @@
+# Ejercicio 1 (10 puntos)
+# Calcule integral de exp(x) entre 0 y 1 con el método de trapecio y de Simpson.
+# Haga una grafica (error.png) del error fraccional entre la solución numérica y 
+# analítica como funcion del numero de puntos (desde N=10 hasta N=10^8). 
+# Tanto el error como el numero de puntos deben variar en escala logaritmica.
+
